8ff858ee55c4d5a3d6dd2aa4b1c47eb0f5a71948
Aryan Dhanwani <aryandhanwani11@gmail.com>
2024-12-30 10:48:40 +0530

    While Loop 1 to 10

---
+++ WHILE1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+// WAP to find 1 to 10 number using while loop
+
+main()
+{
+ int i;
+ clrscr();
+
+ i=1;
+ while(i<=10)
+  {
+   printf("%d\t", i);
+   i++;
+  }
+  getch();
+}
